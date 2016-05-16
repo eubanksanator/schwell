34c25550356eb1dfd9cd2c921f59fa9385d92993
Travis Eubanks <tmeubanks113@gmail.com>
2016-05-16 09:17:58 -0500

    working sms sending through twilio test creds

--- app/controllers/sms_deliveries_controller.rb
+++ app/controllers/sms_deliveries_controller.rb
@@ -2,6 +2,16 @@ class SmsDeliveriesController < ApplicationController
     require 'twilio-ruby'
 
     def send_text
+        twilio_sid = Rails.application.secrets.twilio_sid
+        twilio_token = Rails.application.secrets.twilio_token
 
+        @client = Twilio::REST::Client.new(twilio_sid, twilio_token)
+
+        sms = @client.messages.create(
+            :body => "Here is a test did it work?",
+            :to => "8475088751",
+            :from => "+18475088751")
+
+        redirect_to root_path, :notice => "We sent the text successfully"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'sms_delivery/send_text'
+  get 'sms_deliveries/send_text'
 
   resources :contacts do
     collection do
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     end
   end
 
-  post '/send_my_text' => 'sms_delivery#send_text'
+  post '/send_my_text' => 'sms_deliveries#send_text'
 
   root 'contacts#index'
 
