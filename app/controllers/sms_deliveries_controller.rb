bc75aa90aa74c05a1c68b054536819a62e6f0604
Travis E <teubanks@tutelaholding.com>
2016-05-25 10:22:49 -0500

    adds form for message

--- app/controllers/sms_deliveries_controller.rb
+++ app/controllers/sms_deliveries_controller.rb
@@ -5,33 +5,39 @@ class SmsDeliveriesController < ApplicationController
 
     end
 
-    # def send_text
-    #     account_sid = Rails.application.secrets.twilio_sid
-    #     auth_token = Rails.application.secrets.twilio_token
-    #     @client = Twilio::Rest::Client.new account_sid, auth_token
+    def send_text
+        account_sid = Rails.application.secrets.twilio_sid
+        auth_token = Rails.application.secrets.twilio_token
+        @client = Twilio::Rest::Client.new account_sid, auth_token
 
-    #     contacts = Contact.all
-    #     message_body = params["body"]
+        message_body = params["body"]
+        sms = @client.messages.create(
+            from: '18472326161',
+            to: '8475088751',
+            body: message_body
+            )
 
-    #     contacts.each do |contact|
-    #         sms = @client.messages.create(
-    #             from: # add your twilio number here
-    #             to: contact.phone_num,
-    #             body: message_body
-    #             )
-    #     end
+        # contacts = Contact.all
 
-    #     twilio_sid = Rails.application.secrets.twilio_sid
-    #     twilio_token = Rails.application.secrets.twilio_token
+        # contacts.each do |contact|
+        #     sms = @client.messages.create(
+        #         from: # add your twilio number here
+        #         to: contact.phone_num,
+        #         body: message_body
+        #         )
+        # end
 
-    #     @client = Twilio::REST::Client.new(twilio_sid, twilio_token)
+        # twilio_sid = Rails.application.secrets.twilio_sid
+        # twilio_token = Rails.application.secrets.twilio_token
 
-    #     sms = @client.messages.create(
-    #         :body => "Here is a test did it work?",
-    #         :to => "8475088751",
-    #         :from => "+18475088751")
+        # @client = Twilio::REST::Client.new(twilio_sid, twilio_token)
 
-    #     redirect_to root_path, :notice => "We sent the text successfully"
+        # sms = @client.messages.create(
+        #     :body => "Here is a test did it work?",
+        #     :to => "8475088751",
+        #     :from => "+18475088751")
 
-    # end
+        redirect_to root_path, :notice => "We sent the text successfully"
+
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'sms_deliveries/send_text'
 
   resources :contacts do
     collection do
@@ -8,8 +7,8 @@ Rails.application.routes.draw do
     end
   end
 
+  get 'sms_deliveries/create_message'
 
-  post '/send_my_text' => 'sms_delivery#send_text'
   post '/send_my_text' => 'sms_deliveries#send_text'
 
   root 'contacts#index'
