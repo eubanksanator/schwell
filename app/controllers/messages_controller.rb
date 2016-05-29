ca37f5e7ea974138b998a3f3daf6fcc32845d7d2
Travis Eubanks <tmeubanks113@gmail.com>
2016-05-29 12:38:14 -0500

    adds iteration for all contacts to send out

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,48 +10,40 @@ class MessagesController < ApplicationController
         message_body = params["body"]
         boot_twilio
 
-        sms = @client.messages.create(
-            from: Rails.application.secrets.twilio_number,
-            to: '8475088751',
-            body: message_body
-            )
-
-        # contacts = Contact.all
-
-        # contacts.each do |contact|
-        #     sms = @client.messages.create(
-        #         from: # add your twilio number here
-        #         to: contact.phone_num,
-        #         body: message_body
-        #         )
-        # end
-
-        # twilio_sid = Rails.application.secrets.twilio_sid
-        # twilio_token = Rails.application.secrets.twilio_token
-
-        # @client = Twilio::REST::Client.new(twilio_sid, twilio_token)
-
         # sms = @client.messages.create(
-        #     :body => "Here is a test did it work?",
-        #     :to => "8475088751",
-        #     :from => "+18475088751")
+        #     from: Rails.application.secrets.twilio_number,
+        #     to: '8475088751',
+        #     body: message_body
+        #     )
 
-        redirect_to root_path, :notice => "We sent the text successfully"
+        @contacts = Contact.all
+
+        @successful_msg = 0
+        @contacts.each do |contact|
+            sms = @client.messages.create(
+                from: Rails.application.secrets.twilio_number
+                to: contact.phone_num,
+                body: message_body
+                )
+            @successful_msg += 1
+        end
+
+        redirect_to root_path, :notice => "We sent #{@successful_mg} messages out of #{@contacts} contacts successfully"
 
     end
 
-    def reply
-        message_body = params["Body"] # Responders message
-        from_number  = params["From"] # Responders number
-        boot_twilio
+    # def reply
+    #     message_body = params["Body"] # Responders message
+    #     from_number  = params["From"] # Responders number
+    #     boot_twilio
 
-        sms = @client.messages.create(
-            from: Rails.application.secrets.twilio_number,
-            to: from_number,
-            body: "You responded with #{message_body}"
-            )
-        redirect_to(:back)
-    end
+    #     sms = @client.messages.create(
+    #         from: Rails.application.secrets.twilio_number,
+    #         to: from_number,
+    #         body: "You responded with #{message_body}"
+    #         )
+    #     redirect_to(:back)
+    # end
 
     private
 
